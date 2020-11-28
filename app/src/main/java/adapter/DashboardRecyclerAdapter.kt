d1a0645418a0cf943c7893f9276a5c413834bc79
SHREYA <shreyakoley020102@gmail.com>
2020-11-28 18:45:18 +0530

    BookHub

---
+++ app/src/main/java/adapter/DashboardRecyclerAdapter.kt
@@ -0,0 +1,52 @@
+package adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookhub.R
+import com.example.bookhub.model.Book
+import com.squareup.picasso.Picasso
+
+class DashboardRecyclerAdapter(val context: Context, val itemList: ArrayList<Book>) : RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder> (){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DashboardViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_dasboard_single_row,parent,false)
+
+        return DashboardViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
+        val book = itemList[position]
+        holder.txtBookName.text= book.bookName
+        holder.txtBookAuthor.text = book.bookAuthor
+        holder.txtBookPrice.text = book.bookPrice
+        holder.txtBookRating.text = book.bookRating
+        //holder.imgBookImage.setImageResource(book.bookImage)
+        Picasso.get().load(book.bookImage).into(holder.imgBookImage)
+
+        holder.l1Content.setOnClickListener{
+            Toast.makeText(context, "Clicked on ${holder.txtBookName.text}",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    class DashboardViewHolder (view:View) : RecyclerView.ViewHolder(view){
+        val txtBookName: TextView = view.findViewById(R.id.txtBookName)
+        val txtBookAuthor: TextView = view.findViewById(R.id.txtBookAuthor)
+        val txtBookPrice: TextView = view.findViewById(R.id.txtBookPrice)
+        val txtBookRating: TextView = view.findViewById(R.id.txtBookRating)
+        val imgBookImage: ImageView = view.findViewById(R.id.imgBookImage)
+        val l1Content: LinearLayout = view.findViewById(R.id.l1Content)
+    }
+
+}
+
